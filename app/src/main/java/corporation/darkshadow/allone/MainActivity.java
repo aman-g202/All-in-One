44ef27cccf77e98fe1c95bd50777919567d2721c
aman.gupta <amangupta15jan@gmail.com>
2018-03-18 19:07:01 +0530

    about us layout added

--- app/src/main/java/corporation/darkshadow/allone/MainActivity.java
+++ app/src/main/java/corporation/darkshadow/allone/MainActivity.java
@@ -44,5 +44,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        aboutus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AboutUsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
